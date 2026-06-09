8ced88c2a150a0aa423ff4a7a8a1a0ebc759fd79
EunhoKim <ehkime0423@gmail.com>
2026-06-09 16:51:43 +0900

    QList 제작중...2

--- src/main/java/kh/ddeonabom/qList/controller/QListController.java
+++ src/main/java/kh/ddeonabom/qList/controller/QListController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import kh.ddeonabom.common.paging.PageInfo;
 import kh.ddeonabom.common.paging.Pagination;
@@ -20,14 +21,30 @@ public class QListController {
 	private final QListService qListService;
 	
 	@GetMapping("list")
-	public String selectQList(@RequestParam(value="page", defaultValue="1") int currentPage) {
+	public ModelAndView selectQList(@RequestParam(value="page", defaultValue="1") int currentPage, ModelAndView mv) {
 		int listCount = qListService.getListCount();
 		PageInfo pi = Pagination.getPageInfo(currentPage, listCount, 8, 8);
 		ArrayList<QList> qList = qListService.selectQList(pi);
-		System.out.println(qList);
+//		System.out.println(qList);
 		
-		return "views/qList/qBoard";
+		mv.addObject("qList", qList).addObject("pi", pi).setViewName("views/qList/list");
+		
+		return mv;
 	}
 	
+	@GetMapping("write")
+	public String writeQList() {
+		return "views/qList/write";
+	}
 	
+	@GetMapping("detail")
+	public ModelAndView detailQList(@RequestParam("qNo") int qNo, ModelAndView mv) {
+	    
+	    QList q = qListService.detailQList(1);  // 단건 조회 서비스 호출
+	    System.out.println(q);
+	    mv.addObject("q", q)
+	      .setViewName("views/qList/detail");
+	    
+	    return mv;
+	}
 }
