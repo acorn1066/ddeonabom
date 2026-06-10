eeb1a13a3e395204d86bae1cb5f1ced7f9b8fa44
임동혁 <imdong0919@gmail.com>
2026-06-10 17:37:49 +0900

    feat:후기등록하는중

--- src/main/java/kh/ddeonabom/review/controller/ReviewController.java
+++ src/main/java/kh/ddeonabom/review/controller/ReviewController.java
@@ -5,10 +5,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import kh.ddeonabom.common.paging.PageInfo;
 import kh.ddeonabom.common.paging.Pagination;
@@ -18,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+
 
 public class ReviewController {
 	private final ReviewService reviewService;
@@ -33,25 +37,31 @@ public class ReviewController {
     @ResponseBody 
     public Map<String, Object> selectReviewList(@RequestParam(value="currentPage", defaultValue="1") int currentPage) {
         
-        // [STEP 1] DB에 있는 전체 후기 게시글 개수 가져오기
         int listCount = reviewService.selectListCount(); 
-        
-        // [STEP 2] 한 페이지에 보여줄 번호 수(5개)와 게시글 카드 수(6개) 지정
         int pageLimit = 5;   
         int boardLimit = 9;  
-        
-        // [STEP 3] 공용 Pagination 유틸로 페이징 계산 후 PageInfo(pi) 객체 생성
+
         PageInfo pi = Pagination.getPageInfo(currentPage, listCount, pageLimit, boardLimit);
-        
-        // [STEP 4] 계산된 pi를 서비스로 넘겨서 딱 해당 페이지 분량(6개)만 조회
         ArrayList<Review> list = reviewService.selectReviewList(pi);
-        
-        // [STEP 5] 자바스크립트(Ajax)가 받아서 쓸 수 있게 Map에 묶어서 반환
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("list", list);
         responseData.put("pi", pi);
         
         return responseData; 
     }
+	
+	@GetMapping("/reviews/write")
+	public String reviewWrite(Model model) {
+		 model.addAttribute("kakaoApiKey", "77218df82558088a0b690733061ba6f2");
+		 
+	    return "views/review/write"; 
+	}
 
+	@PostMapping("/reviews/insert")
+	public String insertReviews(@ModelAttribute Review r,@RequestParam("upfile") MultipartFile upfile) {
+		
+		return "redirect:review/list";
+	}
+	
+	
 }
