2e4c1562fd32e68ec6466b80f93421c39885efbe
acorn1066 <acorn1066@gmail.com>
2026-06-10 17:26:48 +0900

    feat:일정추가 관광지 검색 추가

--- src/main/java/kh/ddeonabom/landmark/model/service/LandmarkService.java
+++ src/main/java/kh/ddeonabom/landmark/model/service/LandmarkService.java
@@ -1,12 +1,25 @@
 package kh.ddeonabom.landmark.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
+import kh.ddeonabom.landmark.model.mapper.LandmarkMapper;
+import kh.ddeonabom.landmark.model.vo.Landmark;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class LandmarkService {
+	private final LandmarkMapper mapper;
+	
+	public ArrayList<Landmark> searchLandmarks(String q, int page, int size) {
+	    int offset = page * size;
+	    return mapper.searchLandmarks(q, offset, size);
+	}
 
- 
+	public int countLandmarks(String q) {
+	    return mapper.countLandmarks(q);
+	}
+
 }
